03aa88d438fa9ed952d48cba3bfea6993de69799
bmartin <bmartin@localhost>
2018-06-04 12:50:41 +0200

    Fixing errors in new modules

--- src/app/visitform/visitform.component.ts
+++ src/app/visitform/visitform.component.ts
@@ -17,152 +17,152 @@ import { Global } from '../shared/Global';
   styleUrls: ['./visitform.component.css']
 })
 
+// export class VisitformComponent implements OnInit {
+//   constructor() { }
+//   ngOnInit() {
+//   }
+// }
 export class VisitformComponent implements OnInit {
-  constructor() { }
+  msg: string;
+  indLoading = false;
+  visitFrm: FormGroup;
+  listFilter: string;
+  selectedOption: string;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+  private fb: FormBuilder,
+  private _visitService: VisitService,
+  public dialogRef: MatDialogRef<VisitlistComponent>) { }
+
   ngOnInit() {
+
+  this.visitFrm = this.fb.group({
+    Id: [''],
+    Motivo: ['', [Validators.required, Validators.maxLength(50)]],
+    Duracion: ['', [Validators.required, Validators.email]],
+    ResponsableCatec: ['', [Validators.required]],
+    Fecha: ['', [Validators.required]],
+    Hora: ['', [Validators.required]]
+  });
+
+  // subscribe on value changed event of form to show validation message
+  this.visitFrm.valueChanges.subscribe(data => this.onValueChanged(data));
+  this.onValueChanged();
+
+  if (this.data.dbops === DBOperation.create) {
+    this.visitFrm.reset();
+  } else {
+    this.visitFrm.setValue(this.data.visit);
+  }
+  this.SetControlsState(this.data.dbops === DBOperation.delete ? false : true);
+}
+// form value change event
+onValueChanged(data?: any) {
+  if (!this.visitFrm) { return; }
+  const form = this.visitFrm;
+  // tslint:disable-next-line:forin
+  for (const field in this.formErrors) {
+    // clear previous error message (if any)
+    this.formErrors[field] = '';
+    const control = form.get(field);
+    // setup custom validation message to form
+    if (control && control.dirty && !control.valid) {
+      const messages = this.validationMessages[field];
+      // tslint:disable-next-line:forin
+      for (const key in control.errors) {
+        this.formErrors[field] += messages[key] + ' ';
+      }
+    }
   }
 }
-// export class VisitformComponent implements OnInit {
-//   msg: string;
-//   indLoading = false;
-//   visitFrm: FormGroup;
-//   listFilter: string;
-//   selectedOption: string;
+// form errors model
+// tslint:disable-next-line:member-ordering
+formErrors = {
+  'Motivo': '',
+  'Duracion': '',
+  'ResponsableCatec': '',
+  'Fecha': '',
+  'Hora': ''
+};
+// custom valdiation messages
+// tslint:disable-next-line:member-ordering
+validationMessages = {
+  'Motivo': {
+    'maxlength': 'Motivo cannot be more than 50 characters long.',
+    'required': 'Motivo is required.'
+  },
+  'Duracion': {
+    'maxlength': 'Motivo cannot be more than 50 characters long.',
+    'required': 'Duracion is required.'
+  },
+  'ResponsableCatec': {
+    'maxlength': 'Responsable cannot be more than 50 characters long.',
+    'required': 'Responsable is required.'
+  },
+  'Fecha': {
+    'required': 'Fecha is required.'
+  },
+  'Hora': {
+    'required': 'Hora is required.'
+  }
+};
+onSubmit(formData: any) {
+  const visitData = this.mapDateData(formData.value);
+  switch (this.data.dbops) {
+    case DBOperation.create:
+      this._visitService.addVisit(Global.BASE_USER_ENDPOINTVisit + 'addVisit', visitData).subscribe(
+        data => {
+          // Success
+          if (data.message) {
+            this.dialogRef.close('success');
+          } else {
+            this.dialogRef.close('error');
+          }
+        },
+        error => {
+          this.dialogRef.close('error');
+        }
+      );
+      break;
+    case DBOperation.update:
+      this._visitService.updateVisit(Global.BASE_USER_ENDPOINTVisit + 'updateVisit', visitData.Id, visitData).subscribe(
+        data => {
+          // Success
+          if (data.message) {
+            this.dialogRef.close('success');
+          } else {
+            this.dialogRef.close('error');
+          }
+        },
+        error => {
+          this.dialogRef.close('error');
+        }
+      );
+      break;
+    case DBOperation.delete:
+      this._visitService.deleteVisit(Global.BASE_USER_ENDPOINTVisit + 'deleteVisit', visitData.Id).subscribe(
+        data => {
+          // Success
+          if (data.message) {
+            this.dialogRef.close('success');
+          } else {
+            this.dialogRef.close('error');
+          }
+        },
+        error => {
+          this.dialogRef.close('error');
+        }
+      );
+      break;
+  }
+}
+SetControlsState(isEnable: boolean) {
+  isEnable ? this.visitFrm.enable() : this.visitFrm.disable();
+}
 
-//   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-//   private fb: FormBuilder,
-//   private _visitService: VisitService,
-//   public dialogRef: MatDialogRef<VisitlistComponent>) { }
-
-//   ngOnInit() {
-
-//   this.visitFrm = this.fb.group({
-//     Id: [''],
-//     Motivo: ['', [Validators.required, Validators.maxLength(50)]],
-//     Duracion: ['', [Validators.required, Validators.email]],
-//     ResponsableCatec: ['', [Validators.required]],
-//     Fecha: ['', [Validators.required]],
-//     Hora: ['', [Validators.required]]
-//   });
-
-//   // subscribe on value changed event of form to show validation message
-//   this.visitFrm.valueChanges.subscribe(data => this.onValueChanged(data));
-//   this.onValueChanged();
-
-//   if (this.data.dbops === DBOperation.create) {
-//     this.visitFrm.reset();
-//   } else {
-//     this.visitFrm.setValue(this.data.visit);
-//   }
-//   this.SetControlsState(this.data.dbops === DBOperation.delete ? false : true);
-// }
-// // form value change event
-// onValueChanged(data?: any) {
-//   if (!this.visitFrm) { return; }
-//   const form = this.visitFrm;
-//   // tslint:disable-next-line:forin
-//   for (const field in this.formErrors) {
-//     // clear previous error message (if any)
-//     this.formErrors[field] = '';
-//     const control = form.get(field);
-//     // setup custom validation message to form
-//     if (control && control.dirty && !control.valid) {
-//       const messages = this.validationMessages[field];
-//       // tslint:disable-next-line:forin
-//       for (const key in control.errors) {
-//         this.formErrors[field] += messages[key] + ' ';
-//       }
-//     }
-//   }
-// }
-// // form errors model
-// // tslint:disable-next-line:member-ordering
-// formErrors = {
-//   'Motivo': '',
-//   'Duracion': '',
-//   'ResponsableCatec': '',
-//   'Fecha': '',
-//   'Hora': ''
-// };
-// // custom valdiation messages
-// // tslint:disable-next-line:member-ordering
-// validationMessages = {
-//   'Motivo': {
-//     'maxlength': 'Motivo cannot be more than 50 characters long.',
-//     'required': 'Motivo is required.'
-//   },
-//   'Duracion': {
-//     'maxlength': 'Motivo cannot be more than 50 characters long.',
-//     'required': 'Duracion is required.'
-//   },
-//   'ResponsableCatec': {
-//     'maxlength': 'Responsable cannot be more than 50 characters long.',
-//     'required': 'Responsable is required.'
-//   },
-//   'Fecha': {
-//     'required': 'Fecha is required.'
-//   },
-//   'Hora': {
-//     'required': 'Hora is required.'
-//   }
-// };
-// onSubmit(formData: any) {
-//   const visitData = this.mapDateData(formData.value);
-//   switch (this.data.dbops) {
-//     case DBOperation.create:
-//       this._visitService.addVisit(Global.BASE_USER_ENDPOINTVisit + 'addVisit', visitData).subscribe(
-//         data => {
-//           // Success
-//           if (data.message) {
-//             this.dialogRef.close('success');
-//           } else {
-//             this.dialogRef.close('error');
-//           }
-//         },
-//         error => {
-//           this.dialogRef.close('error');
-//         }
-//       );
-//       break;
-//     case DBOperation.update:
-//       this._visitService.updateVisit(Global.BASE_USER_ENDPOINTVisit + 'updateVisit', visitData.Id, visitData).subscribe(
-//         data => {
-//           // Success
-//           if (data.message) {
-//             this.dialogRef.close('success');
-//           } else {
-//             this.dialogRef.close('error');
-//           }
-//         },
-//         error => {
-//           this.dialogRef.close('error');
-//         }
-//       );
-//       break;
-//     case DBOperation.delete:
-//       this._visitService.deleteVisit(Global.BASE_USER_ENDPOINTVisit + 'deleteVisit', visitData.Id).subscribe(
-//         data => {
-//           // Success
-//           if (data.message) {
-//             this.dialogRef.close('success');
-//           } else {
-//             this.dialogRef.close('error');
-//           }
-//         },
-//         error => {
-//           this.dialogRef.close('error');
-//         }
-//       );
-//       break;
-//   }
-// }
-// SetControlsState(isEnable: boolean) {
-//   isEnable ? this.visitFrm.enable() : this.visitFrm.disable();
-// }
-
-// mapDateData(visit: IVisit): IVisit {
-//   visit.Fecha = new Date(visit.Fecha).toISOString();
-//   return visit;
-// }
-// }
+mapDateData(visit: IVisit): IVisit {
+  visit.Fecha = new Date(visit.Fecha).toISOString();
+  return visit;
+}
+}
 
--- src/app/visitlist/visitlist.component.ts
+++ src/app/visitlist/visitlist.component.ts
@@ -25,7 +25,7 @@ export class VisitlistComponent implements OnInit {
   modalBtnTitle: string;
 
   // set columns that will need to show in listing table
-  displayedColumns = ['Motivo', 'Duracion', 'ResponsableCatec', 'Fecha', 'Hora', 'Action'];
+  displayedColumns = ['Motivo', 'Duracion', 'ResponsableCatec', 'Fecha', 'Hora', 'action'];
   // setting up datasource for material table
   dataSource = new MatTableDataSource<IVisit>();
 
@@ -35,35 +35,35 @@ export class VisitlistComponent implements OnInit {
     this.loadingStateVisit = true;
     this.loadVisits();
   }
-  // openDialog(): void {
-  //   const dialogRef = this.dialog.open(VisitformComponent, {
-  //     width: '500px',
-  //     data: { dbops: this.dbops, modalTitle: this.modalTitle, modalBtnTitle: this.modalBtnTitle, visit: this.visit }
-  //   });
+  openDialog(): void {
+    const dialogRef = this.dialog.open(VisitformComponent, {
+      width: '500px',
+      data: { dbops: this.dbops, modalTitle: this.modalTitle, modalBtnTitle: this.modalBtnTitle, visit: this.visit }
+    });
 
-  //   dialogRef.afterClosed().subscribe(result => {
-  //     console.log('The dialog was closed');
-  //     if (result === 'success') {
-  //       this.loadingState = true;
-  //       this.loadVisits();
-  //       switch (this.dbops) {
-  //         case DBOperation.create:
-  //           this.showMessage('Data successfully added.');
-  //           break;
-  //         case DBOperation.update:
-  //           this.showMessage('Data successfully updated.');
-  //           break;
-  //         case DBOperation.delete:
-  //           this.showMessage('Data successfully deleted.');
-  //           break;
-  //       }
-  //     } else if (result === 'error') {
-  //       this.showMessage('There is some issue in saving records, please contact to system administrator!');
-  //     } else {
-  //      // this.showMessage('Please try again, something went wrong');
-  //     }
-  //   });
-  // }
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if (result === 'success') {
+        this.loadingStateVisit = true;
+        this.loadVisits();
+        switch (this.dbops) {
+          case DBOperation.create:
+            this.showMessage('Data successfully added.');
+            break;
+          case DBOperation.update:
+            this.showMessage('Data successfully updated.');
+            break;
+          case DBOperation.delete:
+            this.showMessage('Data successfully deleted.');
+            break;
+        }
+      } else if (result === 'error') {
+        this.showMessage('There is some issue in saving records, please contact to system administrator!');
+      } else {
+       // this.showMessage('Please try again, something went wrong');
+      }
+    });
+  }
 
   loadVisits(): void {
     this._visitService.getAllVisit(Global.BASE_USER_ENDPOINTVisit + 'getAllVisit')
@@ -72,26 +72,26 @@ export class VisitlistComponent implements OnInit {
         this.dataSource.data = visits;
       });
   }
-  // addVisit() {
-  //   this.dbops = DBOperation.create;
-  //   this.modalTitle = 'Add New Visit';
-  //   this.modalBtnTitle = 'Add';
-  //   this.openDialog();
-  // }
-  // editVisit(Id: number) {
-  //   this.dbops = DBOperation.update;
-  //   this.modalTitle = 'Edit Visit';
-  //   this.modalBtnTitle = 'Update';
-  //   this.visit = this.dataSource.data.filter(x => x.Id === Id)[0];
-  //   this.openDialog();
-  // }
-  // deleteVisit(Id: number) {
-  //   this.dbops = DBOperation.delete;
-  //   this.modalTitle = 'Confirm to Delete ?';
-  //   this.modalBtnTitle = 'Delete';
-  //   this.visit = this.dataSource.data.filter(x => x.Id === Id)[0];
-  //   this.openDialog();
-  // }
+  addVisit() {
+    this.dbops = DBOperation.create;
+    this.modalTitle = 'Add New Visit';
+    this.modalBtnTitle = 'Add';
+    this.openDialog();
+  }
+  editVisit(Id: number) {
+    this.dbops = DBOperation.update;
+    this.modalTitle = 'Edit Visit';
+    this.modalBtnTitle = 'Update';
+    this.visit = this.dataSource.data.filter(x => x.Id === Id)[0];
+    this.openDialog();
+  }
+  deleteVisit(Id: number) {
+    this.dbops = DBOperation.delete;
+    this.modalTitle = 'Confirm to Delete ?';
+    this.modalBtnTitle = 'Delete';
+    this.visit = this.dataSource.data.filter(x => x.Id === Id)[0];
+    this.openDialog();
+  }
   showMessage(msg: string) {
     this.snackBar.open(msg, '', {
       duration: 3000
