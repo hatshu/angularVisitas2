27ef1955bf37e720a0a92baa7a01e2057ed3bb12
bmartin <bmartin@localhost>
2018-08-16 08:40:52 +0200

    Obtain only active users of group CTC-TODOS in Controller

--- Controllers/ADController.cs
+++ Controllers/ADController.cs
@@ -21,21 +21,53 @@ namespace contact_app.Controllers {
     [Route ("getAllUsers")]
     public IEnumerable<string> GetAllUsers () {
       // fetch all contact records
-      var myDomainUsers = new List<string> ();
-      using (var ctx = new PrincipalContext (ContextType.Domain, "catec.local")) {
-        var userPrinciple = new UserPrincipal (ctx);
-        using (var search = new PrincipalSearcher (userPrinciple)) {
-          foreach (var domainUser in search.FindAll ()) {
-            if (domainUser.DisplayName != null) {
-              myDomainUsers.Add (domainUser.DisplayName);
-            }
+      // var myDomainUsers = new List<string> ();
+      // using (var ctx = new PrincipalContext (ContextType.Domain, "catec.local")) {
+      //   var userPrinciple = new UserPrincipal (ctx);
+      //   using (var search = new PrincipalSearcher (userPrinciple)) {
+      //     foreach (var domainUser in search.FindAll ()) {
+      //       if (domainUser.DisplayName != null) {
+      //         myDomainUsers.Add (domainUser.DisplayName);
+      //       }
+      //     }
+      //   }
+      // }
+      //       return myDomainUsers;
+      // set up domain context
+PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
+var myDomainUsers = new List<string> ();
+
+// find the group in question
+GroupPrincipal group = GroupPrincipal.FindByIdentity(ctx, "CTC-TODOS");
+
+// if found....
+if (group != null)
+{
+   // iterate over members
+   foreach (Principal p in group.GetMembers())
+   {
+      Console.WriteLine("{0}: {1}", p.StructuralObjectClass, p.DisplayName);
+
+      // do whatever you need to do to those members
+      UserPrincipal theUser = p as UserPrincipal;
+
+      if(theUser != null)
+      {
+          if(theUser.IsAccountLockedOut())
+          {
+           //ESTA DE BAJA;
+           Console.WriteLine (" Esta de baja "+ p.DisplayName);
           }
-        }
+          else
+          {
+            myDomainUsers.Add(theUser.DisplayName);
+          }
       }
-            return myDomainUsers;
+   }
+}
+  return myDomainUsers;
 
     }
-
 
   }
 }
